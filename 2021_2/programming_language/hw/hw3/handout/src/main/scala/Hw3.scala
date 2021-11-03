b841a9a022150e0b8018651126d83e1f1f3b164e
minjae-lulu <79103625+minjae-lulu@users.noreply.github.com>
2021-11-03 17:39:42 +0900

    update

---
+++ 2021_2/programming_language/hw/hw3/handout/src/main/scala/Hw3.scala
@@ -0,0 +1,77 @@
+package hw3
+
+import scala.collection.immutable.HashMap 
+import hw3._
+
+
+package object hw3 {
+  type Env = HashMap[Var,Val]
+  type Loc = Int
+  
+}
+
+case class Mem(m: HashMap[Loc,Val], top: Loc) {
+
+}
+
+sealed trait Val
+case class IntVal(n: Int) extends Val
+case class IntListVal(n: List[IntVal]) extends Val
+case class BoolVal(b: Boolean) extends Val
+case class ProcVal(v: Var, expr: Expr, env: Env) extends Val
+case class RecProcVal(fv: Var, av: Var, body: Expr, env: Env) extends Val
+case class LocVal(l: Loc) extends Val
+
+
+sealed trait Program
+sealed trait Expr extends Program
+case class ConstI(n: Int) extends Expr
+case class ConstB(n: Boolean) extends Expr
+case class ConstIL(n: List[IntVal]) extends Expr
+case class Var(s: String) extends Expr
+case class Add(l: Expr, r: Expr) extends Expr
+case class Sub(l: Expr, r: Expr) extends Expr
+case class Mul(l: Expr, r: Expr) extends Expr
+case class Div(l: Expr, r: Expr) extends Expr
+case class Rem(l: Expr, r: Expr) extends Expr
+case class Cons(l: Expr, r: Expr) extends Expr
+case class GTExpr(l: Expr, r: Expr) extends Expr
+case class GEQExpr(l: Expr, r: Expr) extends Expr
+case class Iszero(c: Expr) extends Expr
+case class Ite(c: Expr, t: Expr, f: Expr) extends Expr
+case class ValExpr(name: Var, value: Expr, body: Expr) extends Expr
+case class VarExpr(name: Var, value: Expr, body: Expr) extends Expr
+case class Proc(v: Var, expr: Expr) extends Expr
+case class DefExpr(fname: Var, aname: Var, fbody: Expr, ibody: Expr) extends Expr
+case class Asn(v: Var, e: Expr) extends Expr
+case class Paren(expr: Expr) extends Expr
+case class Block(f: Expr, s: Expr) extends Expr
+case class PCall(ftn: Expr, arg: Expr) extends Expr
+
+
+
+
+
+
+
+object MiniScalaInterpreter {
+
+  case class Result(v: Val, m: Mem)
+  case class UndefinedSemantics(msg: String = "", cause: Throwable = None.orNull) extends Exception("Undefined Semantics: " ++ msg, cause)
+  
+  
+  def eval(env: Env, mem: Mem, expr: Expr): Result = Result(BoolVal(false), Mem(new HashMap[Loc,Val],0))
+  
+  def apply(program: String): Val = {
+    val parsed = MiniScalaParserDriver(program)
+    eval(new Env(), Mem(new HashMap[Loc,Val],0), parsed).v
+  }
+
+}
+
+
+object Hw3App extends App {
+  
+  println("Hello from Hw3!")
+
+}
