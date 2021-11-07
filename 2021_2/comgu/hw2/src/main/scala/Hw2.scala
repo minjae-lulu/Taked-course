6651c8fa420e0493ca7578854bf83015514ecdb1
minjae-lulu <79103625+minjae-lulu@users.noreply.github.com>
2021-11-07 21:19:20 +0900

    update

--- 2021_2/comgu/hw2/src/main/scala/Hw2.scala
+++ 2021_2/comgu/hw2/src/main/scala/Hw2.scala
@@ -49,15 +49,25 @@ class ALU extends Module {
   io.res := 0.U
   io.zero := true.B
 
+  when(io.a >= io.b && io.a <= io.b){
+    io.zero := true.B
+  }.otherwise{
+    io.zero := false.B
+  }
+
+  switch(io.ctrl){
+    is(AluCtrl.and) {io.res := io.a & io.b}
+    is(AluCtrl.or) {io.res := io.a | io.b}
+    is(AluCtrl.add) {io.res := io.a + io.b}
+    is(AluCtrl.sub) {io.res := io.a - io.b}
+  }
+  
   /*Your code ends here */
-
-
 }
 
 /*
 Task 2: ImmGen
 */
-
 
 class ImmGen extends Module {
   val io = IO(new Bundle {
@@ -66,7 +76,22 @@ class ImmGen extends Module {
   })
 
   /* Your code starts here*/
+
   io.imm := 0.U
+  //io.imm := Cat(io.insn(3,1),io.insn(22,10))
+
+  val opco = io.insn(6,0)
+  val inst = io.insn
+
+  switch(opco){
+    is(OpCode.addi) { io.imm := Cat( Fill(53, inst(31)), inst(31,20)) }
+    //is(OpCode.add)
+    is(OpCode.sd)   { io.imm := Cat( Fill(53, inst(31)), inst(31,25), inst(11,7)) }
+    is(OpCode.ld)   { io.imm := Cat( Fill(53, inst(31)), inst(31,20)) }
+    is(OpCode.beq)  { io.imm := Cat( Fill(53, inst(31)), inst(31), inst(7), inst(30,25), inst(11,8)) }
+    
+  }
+  
 
   /*Your code ends here */
 
@@ -87,6 +112,15 @@ class ALUControl extends Module {
   /* Your code starts here*/
   io.aluCtrl := 0.U
 
+  when(io.aluOp === 0.U ){
+    io.aluCtrl := 2.U
+  }.elsewhen(io.aluOp === 1.U){
+    io.aluCtrl := 6.U
+  }.elsewhen(io.aluOp === 2.U){
+    io.aluCtrl := 2.U
+  }
+
+
   /*Your code ends here */
 
 }
@@ -106,21 +140,65 @@ class Control extends Module {
     val branch = Output(Bool())
   })
 /* Your code starts here*/
-    io.aluOp := 0.U
-
+  io.aluOp := 0.U
   io.write_reg := false.B
-
   io.aluSrcFromReg := false.B
-
   io.memWrite := false.B
-
   io.memToReg := false.B
-
   io.branch := false.B
 
+val addi  = "b0010011".U
+  val add   = "b0110011".U
+  val sd    = "b0100011".U
+  val ld    = "b0000011".U
+  val beq   = "b1100011".U
+
+when(io.in === OpCode.addi){
+  io.write_reg := true.B 
+  io.aluSrcFromReg := false.B //
+  io.memWrite := false.B 
+  io.memToReg := false.B 
+  io.aluOp := "b10".U
+  io.branch := false.B 
+}
+
+when(io.in === OpCode.add){
+  io.write_reg := true.B 
+  io.aluSrcFromReg := true.B //
+  io.memWrite := false.B 
+  io.memToReg := false.B 
+  io.aluOp := "b10".U
+  io.branch := false.B 
+}
+
+when(io.in === OpCode.sd){
+  io.write_reg := false.B 
+  io.aluSrcFromReg := false.B //
+  io.memWrite := true.B 
+  io.memToReg := false.B 
+  io.aluOp := "b00".U 
+  io.branch := false.B 
+}
+
+when(io.in === OpCode.ld){
+  io.write_reg := true.B 
+  io.aluSrcFromReg := false.B //
+  io.memWrite := false.B 
+  io.memToReg := true.B 
+  io.aluOp := "b00".U 
+  io.branch := false.B 
+}
+
+when(io.in === OpCode.beq){
+  io.write_reg := false.B 
+  io.aluSrcFromReg := false.B //
+  io.memWrite := false.B 
+  io.memToReg := false.B 
+  io.aluOp := "b01".U 
+  io.branch := true.B 
+}
 
   /*Your code ends here */
 
   
 }
-
